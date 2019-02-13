9c211e8b7d8792366e67619625ceecbb7d7feb29
tinghao.ma <tinghao.ma@ele.me>
2019-02-13 18:59:45 +0800

    Symbol.iterator

--- src/util/a/c.js
+++ src/util/a/c.js
@@ -1,17 +1,27 @@
 {
-  let controller = {
-    a: 100,
-    makeRequest: function() {
-      setTimeout(() => {
-        console.log(this);
-        console.log(this.a);
-      }, 1000)
-    },
-    test: function() {
-      console.log(this);
-      console.log(this.a);
-    }
-  }
-  controller.makeRequest()
-  controller.test()
+  // let controller = {
+  //   a: 100,
+  //   makeRequest: function() {
+  //     setTimeout(() => {
+  //       console.log(this);
+  //       console.log(this.a);
+  //     }, 0)
+  //   },
+  //   test: function() {
+  //     console.log(this);
+  //     console.log(this.a);
+  //   }
+  // }
+  // controller.makeRequest()
+  // controller.test()
+}
+
+{
+  let array = [1, 2, 3]
+  const values = array[Symbol.iterator]()
+  console.log(values);
+  console.log(values.next());
+  console.log(values.next());
+  console.log(values.next());
+  console.log(values.next());
 }
